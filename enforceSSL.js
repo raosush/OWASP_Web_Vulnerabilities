5703a128cffc2f41b71da524d28c9e83bf965228
Sushanth Sathesh Rao <rao.sush004@gmail.com>
2021-03-15 22:39:10 +0530

    Enforce SSL

---
+++ enforceSSL.js
@@ -0,0 +1,11 @@
+const enforceSSL = (req, res, next) => {
+    if (process.env.NODE_ENV === "production") {
+        req.headers["x-forwarded-proto"] !== "https"
+        ? res.redirect(301, "https://" + req.hostname + req.originalUrl)
+        : next();
+    } else {
+        next();
+    }
+}
+
+exports.default = enforceSSL;
